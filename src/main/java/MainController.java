a5e1201301e8676bcf1635636cf928de52267c7d
Raúl <tpautnfrm@gmail.com>
2022-11-02 20:37:31 -0300

    added Controller

---
+++ src/main/java/MainController.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping(value = "/inicio")
+    public String method(){
+
+        return "otro ejemplo";
+    }
+
+}
